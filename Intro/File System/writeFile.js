5171da410f809db941bc15f5faebdcd308cf4245
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2020-03-09 11:01:16 +0100

    writfile

---
+++ Intro/File System/writeFile.js
@@ -0,0 +1,12 @@
+// Importing th file system library
+const fs = require('fs');
+
+// write a file:
+const data = "This is the Text what Node js writet to..."
+fs.writeFile("./data.txt", data, (error=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("data.txt was created successfuly")
+    }
+}))
