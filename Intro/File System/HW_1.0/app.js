a7968c48078dba50cab475609ae24bb36df62b36
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2020-03-09 15:54:45 +0100

    HW_1.0 added

---
+++ Intro/File System/HW_1.0/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req,res)=>{
+    // TODOO
+}).listen(1337, ()=>{
+    console.log(`The Server Is Running On Port 1337`)
+});
+
+
